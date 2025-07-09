c293ca9654d74406cd2f953d1b2a2dd4c2d9bb56
Md Iqbal Hossain <mdiqbalhossainj@gmail.com>
2025-07-09 12:29:15 -0500

    seed 50

--- train_washu2_classifier_multihead.py
+++ train_washu2_classifier_multihead.py
@@ -19,7 +19,7 @@ from utils import plot_roc_curve
 import numpy as np
 import random 
 
-SEED = 99 
+SEED = 50 
 np.random.seed(SEED); torch.manual_seed(SEED) ; random.seed(SEED); 
 
 max_epochs = 250
